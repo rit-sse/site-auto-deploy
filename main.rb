07c60c4d65d1959ac3c915eadf5d21e19cd5b097
7imbrook <timbrook480@gmail.com>
2014-02-28 09:23:32 -0500

    Cleaned up deploy script a little

--- main.rb
+++ main.rb
@@ -1,18 +1,46 @@
 require 'sinatra'
 require 'jekyll'
 require 'JSON'
+require 'netaddr'
+require 'net/http'
+
+# Set up and globals
+configure do
+  set :DEPLOY_DIR, "/User/michael/test-site"
+
+  # This is slow and probably not needed
+  puts "Getting address information..."
+  ip = Net::HTTP.get(URI.parse('http://ifconfig.me/ip'))
+  host = Net::HTTP.get(URI.parse('http://ifconfig.me/host'))
+
+  ip.delete!("\n")
+  host.delete!("\n")
+
+  puts "Have your webhook point to #{host}:#{settings.port} - #{ip}:#{settings.port}"
+  puts "Your site will deploy to #{settings.DEPLOY_DIR}"
+
+end
 
 post '/' do
+
   body = JSON.parse request.body.read
   user_agent = request.user_agent
 
-  if user_agent =~ /^GitHub Hookshot/ and body["ref"] == "refs/heads/master"
+  check_ip = Net::HTTP.get(URI.parse('https://api.github.com/meta'))
+  valid_ips = JSON.parse(check_ip)
+  mask = valid_ips["hooks"]
+  puts mask
+  netmask = NetAddr::CIDR.create(mask)
+
+  # Make sure it's github!
+  if netmask.contains?(request.ip) and user_agent =~ /^GitHub Hookshot/ and body["ref"] == "refs/heads/master"
     puts "Starting Deplay"
 
     # To the temp dir! And clone
     tmpdir = "/tmp/" + body["after"]
     system "git clone https://github.com/rit-sse/crazy-train.git #{tmpdir}"
     Dir.chdir(tmpdir)
+
 
     # Get the deps
     system "bundle install"
@@ -21,7 +49,7 @@ post '/' do
     # Build the site
     conf = Jekyll.configuration({
       'source'      => tmpdir,
-      'destination' => '/Users/michael/test-site/'
+      'destination' => settings.DEPLOY_DIR
     })
     Jekyll::Site.new(conf).process
 
