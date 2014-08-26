65044fc8252e4d8a4b1529ae946e7ea3f3186b79
kristenmills <kristen@kristen-mills.com>
2014-08-25 20:28:42 -0400

    Updated to use github secrets instead of user agent

--- main.rb
+++ main.rb
@@ -7,6 +7,9 @@ require 'socket'
 require  'yaml'
 require 'bundler/setup'
 require 'mail'
+require 'dotenv'
+require 'openssl'
+Dotenv.load
 
 # Set up and globals and process things
 configure do
@@ -55,7 +58,7 @@ end
 # The bit that make it happen
 post '/' do
 
-  body = JSON.parse request.body.read
+  body = request.body.read
   user_agent = request.user_agent
   url = URI.parse('https://api.github.com/meta')
   http = Net::HTTP.new(url.host, url.port)
@@ -67,47 +70,47 @@ post '/' do
   netmask = NetAddr::CIDR.create(mask)
 
   # Make sure it's github!
-  if user_agent =~ /^GitHub Hookshot/
-    if( request.env['HTTP_X_GITHUB_EVENT'] == 'push' and settings.hooks.include?({body['repository']['name'] => body['ref']}) )
-      puts "Starting Deploy..."
-      Bundler.with_clean_env do
-        # Get the deps
-        Dir.chdir('/web') do
-          system 'git pull'
-          system 'git submodule foreach git pull origin master'
-          system 'bundle install'
-          system 'npm install'
+  verify_signature(body)
+  body = JSON.parse(body)
+  if( request.env['HTTP_X_GITHUB_EVENT'] == 'push' and settings.hooks.include?({body['repository']['name'] => body['ref']}) )
+    puts "Starting Deploy..."
+    Bundler.with_clean_env do
+      # Get the deps
+      Dir.chdir('/web') do
+        system 'git pull'
+        system 'git submodule foreach git pull origin master'
+        system 'bundle install'
+        system 'npm install'
 
-          Dir.chdir('governing-docs') do
-            constitution = File.open('constitution.md').read
-            popol = File.open('primary-officers-policy.md').read
-            constitution = "---\nlayout: page\ntitle: \nsidebars: _constitution.html\npermalink: constitution/\n---\n#{constitution}"
-            popol = "---\nlayout: page\ntitle: \nsidebars:\n- _constitution.html\npermalink: primary-officers-policy/\n---\n#{popol}"
+        Dir.chdir('governing-docs') do
+          constitution = File.open('constitution.md').read
+          popol = File.open('primary-officers-policy.md').read
+          constitution = "---\nlayout: page\ntitle: \nsidebars: _constitution.html\npermalink: constitution/\n---\n#{constitution}"
+          popol = "---\nlayout: page\ntitle: \nsidebars:\n- _constitution.html\npermalink: primary-officers-policy/\n---\n#{popol}"
 
-            File.open('constitution.md', 'w') {|f| f.write(constitution)}
-            File.open('primary-officers-policy.md', 'w'){|f| f.write(popol)}
-          end
-          FileUtils.rm_r('assets/images/pages', force: true)
-          FileUtils.cp_r('pages/images/.', 'assets/images/pages')
+          File.open('constitution.md', 'w') {|f| f.write(constitution)}
+          File.open('primary-officers-policy.md', 'w'){|f| f.write(popol)}
+        end
+        FileUtils.rm_r('assets/images/pages', force: true)
+        FileUtils.cp_r('pages/images/.', 'assets/images/pages')
 
-          FileUtils.rm_r('assets/images/posts', force: true)
-          FileUtils.cp_r('_posts/images/.', 'assets/images/posts')
-          puts "Building site..."
-          success = system "jekyll build --source #{settings.src_dir} --destination /tmp/#{body['head_commit']['id']}"
-          if(success)
-            FileUtils.rm_r(Dir.glob("#{settings.deploy_dir}/*"))
-            FileUtils.cp_r("/tmp/#{body['head_commit']['id']}/.", settings.deploy_dir)
-          else
-            Mail.deliver do
-              from     'noreply@sse.se.rit.edu'
-              to       body["pusher"]["email"]
-              subject  'The site failed to build'
-              body     "Your most recent push to the website didn't properly build. Test it locally and fix your mistake."
-            end
+        FileUtils.rm_r('assets/images/posts', force: true)
+        FileUtils.cp_r('_posts/images/.', 'assets/images/posts')
+        puts "Building site..."
+        success = system "jekyll build --source #{settings.src_dir} --destination /tmp/#{body['head_commit']['id']}"
+        if(success)
+          FileUtils.rm_r(Dir.glob("#{settings.deploy_dir}/*"))
+          FileUtils.cp_r("/tmp/#{body['head_commit']['id']}/.", settings.deploy_dir)
+        else
+          Mail.deliver do
+            from     'noreply@sse.se.rit.edu'
+            to       body["pusher"]["email"]
+            subject  'The site failed to build'
+            body     "Your most recent push to the website didn't properly build. Test it locally and fix your mistake."
           end
-          Dir.chdir('governing-docs') do
-            system "git checkout ."
-          end
+        end
+        Dir.chdir('governing-docs') do
+          system "git checkout ."
         end
       end
     end
@@ -115,4 +118,10 @@ post '/' do
 
   # KK we good
   "ok"
+end
+
+# Verify that it's from github
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
 end
